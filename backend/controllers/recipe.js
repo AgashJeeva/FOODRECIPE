0a6306517003752c3b49286bc944acc9df4c373f
AgashJeeva <agashbilla2001@gmail.com>
2026-04-07 15:24:41 +0530

    Create routes, controller

---
+++ backend/controllers/recipe.js
@@ -0,0 +1,21 @@
+const getRecipes = (req, res) => {
+  res.send('Hello World!');
+};
+
+const getRecipe = (req, res) => {
+  res.send('Hello World!');
+};
+
+const addRecipe = (req, res) => {
+  res.send('Hello World!');
+};
+
+const editRecipe = (req, res) => {
+  res.send('Hello World!');
+};
+
+const deleteRecipe = (req, res) => {
+  res.send('Hello World!');
+};
+
+module.exports = { getRecipes, getRecipe, addRecipe, editRecipe, deleteRecipe };
---
+++ backend/routes/recipe.js
@@ -0,0 +1,12 @@
+const  express = require('express');
+const { getRecipes, getRecipe, addRecipe, editRecipes, deleteRecipe } = require('../controllers/recipe');
+const router = express.Router();
+
+router.get('/',getRecipes); //get all recipes
+router.get('/:id',getRecipe); //get recipe by id  
+router.post('/',addRecipe); //create a new recipe
+router.put('/:id',editRecipe); //update a recipe by id
+router.delete('/:id',deleteRecipe); //delete a recipe by id   
+
+
+module.exports = router;
